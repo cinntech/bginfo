f4e56e072c57310c3cbbb8a78caa5a528b5df8f6
Wallace Cinnamon <78132997+cinntech@users.noreply.github.com>
2025-03-30 12:26:48 -0400

    Update Startup-Shortcut.ps1
    added delay to deal with network stack isn’t fully initialized at login

--- Startup-Shortcut.ps1
+++ Startup-Shortcut.ps1
@@ -3,10 +3,10 @@ $bgiFile = "C:\ProgramData\BGInfo\CinnTech-Hostname.bgi"
 $startupFolder = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 $shortcutPath = "$startupFolder\BGInfo.lnk"
 
-# Create a shortcut to run BGInfo silently at startup
+# Create a shortcut to run BGInfo with delay at startup
 $WScriptShell = New-Object -ComObject WScript.Shell
 $shortcut = $WScriptShell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = $bginfoExe
-$shortcut.Arguments = "`"$bgiFile`" /silent /accepteula /timer:0"
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-WindowStyle Hidden -Command `"Start-Sleep 45; Start-Process '`"$bginfoExe`"' -ArgumentList '`"$bgiFile`" /silent /accepteula /timer:0'`""
 $shortcut.WorkingDirectory = "C:\ProgramData\BGInfo"
 $shortcut.Save()
